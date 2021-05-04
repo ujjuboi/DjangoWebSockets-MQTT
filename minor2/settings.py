aaed81bee8e723815ba769efead7ad80f3905ab0
Shreya Maheshwari <maheshwarishreya99@gmail.com>
2021-05-05 00:38:37 +0530

    Home page created

--- minor2/settings.py
+++ minor2/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'firstpage',
     'graph',
+
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

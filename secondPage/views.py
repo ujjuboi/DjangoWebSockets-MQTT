c44cd8162ad55dbce5c2159d206c998ea5fe494f
Shreya Maheshwari <maheshwarishreya99@gmail.com>
2021-05-08 22:50:07 +0530

    addition of sensors to user's list added

--- secondPage/views.py
+++ secondPage/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import redirect, render
 from .models import SensorData, UserSensor
 from .forms import UserMapForm
+from django.db.models import Q
 # Create your views here.
 
 def second(request):
     if(request.user.is_authenticated):
-        # userD = UserSensor.objects.filter(Uid = request.user.id)
-        
         form = UserMapForm(request.POST or None, initial = {"Uid" : request.user.id})
         if(form.is_valid and request.POST ):
             form.save()
+        userD = UserSensor.objects.filter(Uid = request.user.id)
         context = {
             'user' : request.user,
             'sensorData' : SensorData.objects.all(),
-            # 'userSensor' : userD,
+            'SelectedSensor' : userD,
             'form' : form,
         }
         return render(request, 'secondP.html', context)

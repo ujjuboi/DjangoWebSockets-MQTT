892bf9ca8549fa9309cef97a91eb5fa0f67219e7
Shreya Maheshwari <maheshwarishreya99@gmail.com>
2021-05-10 14:18:27 +0530

    Added delete functionality, improved sensor selection form

--- secondPage/views.py
+++ secondPage/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import redirect, render
 from .models import SensorData, UserSensor
 from .forms import UserMapForm
 from django.db.models import Q
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
 # Create your views here.
-
+@csrf_exempt
 def second(request):
+    if(request.method == 'POST'):
+        data = request.POST
+        method = request.POST.get("_method")
+        if method == "delete":
+            delItem = data.get('userValue')
+            x = UserSensor.objects.get(pk = delItem)
+            x.delete()
+            return redirect("/second")
     if(request.user.is_authenticated):
         form = UserMapForm(request.POST or None, initial = {"Uid" : request.user.id})
-        if(form.is_valid and request.POST ):
+        if(form.is_valid and request.POST  and request.method == 'POST'):
             form.save()
+            print(form.cleaned_data)
+            return redirect("/second")
         userD = UserSensor.objects.filter(Uid = request.user.id)
         context = {
             'user' : request.user,

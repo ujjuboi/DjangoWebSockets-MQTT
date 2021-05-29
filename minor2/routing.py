25ba6f25dac94b8d403b72958cf87bc56a8c03d5
Shreya Maheshwari <maheshwarishreya99@gmail.com>
2021-05-29 21:09:16 +0530

    User auth added, Group formation added, firstpage, secondpage, templates modified

--- minor2/routing.py
+++ minor2/routing.py
@@ -1,14 +1,16 @@
-from django.urls import path    #for specifying path
+from django.urls import path, re_path    #for specifying path
 from channels.routing import URLRouter, ProtocolTypeRouter  #for websocket protocol routing
 from channels.auth import AuthMiddlewareStack   #authentication
 
 #add consumers here
 from graph.consumer import Graph
 from firstpage.consumer import Firstpage
+from secondPage.consumer import ChatConsumer
 
 ws_pattern = [
     path('ws/home/', Firstpage.as_asgi()),
     path('ws/graph/', Graph.as_asgi()),
+    re_path(r'ws/data/(?P<room_name>\w+)/(?P<auth_token>\S+)/$',ChatConsumer.as_asgi()),
 ]
 
 application = ProtocolTypeRouter(
